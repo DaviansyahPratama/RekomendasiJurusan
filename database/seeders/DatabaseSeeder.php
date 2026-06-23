8ad36dbd0aecbe11328e9b2aeaccb8514027aeb2
firdausfikriii <firdaus24ti@mahasiswa.pcr.ac.id>
2026-06-23 23:00:17 +0700

    ganti seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,12 +25,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $minatList = [
-            'Pemrograman',
-            'Jaringan',
-            'Data Science',
-            'Artificial Intelligence',
-            'Cyber Security',
-            'Multimedia',
+            'Rekayasa Perangkat Lunak',
+            'Kecerdasan Buatan dan Data',
+            'Jaringan dan Keamanan Siber',
         ];
 
         foreach ($minatList as $nama) {
@@ -38,19 +35,20 @@ class DatabaseSeeder extends Seeder
         }
 
         $mataKuliahData = [
-            ['kode_mk' => 'IF101', 'nama_mk' => 'Algoritma dan Pemrograman', 'sks' => 3, 'semester' => 1, 'kategori' => 'Pemrograman', 'tingkat_kesulitan' => 2],
-            ['kode_mk' => 'IF102', 'nama_mk' => 'Matematika Diskrit', 'sks' => 3, 'semester' => 1, 'kategori' => 'Pemrograman', 'tingkat_kesulitan' => 2],
-            ['kode_mk' => 'IF201', 'nama_mk' => 'Struktur Data', 'sks' => 3, 'semester' => 2, 'kategori' => 'Pemrograman', 'tingkat_kesulitan' => 3],
-            ['kode_mk' => 'IF202', 'nama_mk' => 'Basis Data', 'sks' => 3, 'semester' => 2, 'kategori' => 'Pemrograman', 'tingkat_kesulitan' => 3],
-            ['kode_mk' => 'IF301', 'nama_mk' => 'Pemrograman Web', 'sks' => 3, 'semester' => 3, 'kategori' => 'Pemrograman', 'tingkat_kesulitan' => 3],
-            ['kode_mk' => 'IF302', 'nama_mk' => 'Jaringan Komputer', 'sks' => 3, 'semester' => 3, 'kategori' => 'Jaringan', 'tingkat_kesulitan' => 3],
-            ['kode_mk' => 'IF303', 'nama_mk' => 'Analisis Data', 'sks' => 3, 'semester' => 3, 'kategori' => 'Data Science', 'tingkat_kesulitan' => 3],
-            ['kode_mk' => 'IF401', 'nama_mk' => 'Machine Learning', 'sks' => 3, 'semester' => 4, 'kategori' => 'Data Science', 'tingkat_kesulitan' => 4],
-            ['kode_mk' => 'IF402', 'nama_mk' => 'Kecerdasan Buatan', 'sks' => 3, 'semester' => 4, 'kategori' => 'Artificial Intelligence', 'tingkat_kesulitan' => 4],
-            ['kode_mk' => 'IF403', 'nama_mk' => 'Keamanan Siber', 'sks' => 3, 'semester' => 4, 'kategori' => 'Cyber Security', 'tingkat_kesulitan' => 4],
-            ['kode_mk' => 'IF404', 'nama_mk' => 'Desain Multimedia', 'sks' => 3, 'semester' => 4, 'kategori' => 'Multimedia', 'tingkat_kesulitan' => 2],
-            ['kode_mk' => 'IF501', 'nama_mk' => 'Deep Learning', 'sks' => 3, 'semester' => 5, 'kategori' => 'Artificial Intelligence', 'tingkat_kesulitan' => 5],
-            ['kode_mk' => 'IF502', 'nama_mk' => 'Administrasi Jaringan', 'sks' => 3, 'semester' => 5, 'kategori' => 'Jaringan', 'tingkat_kesulitan' => 4],
+            ['kode_mk' => 'IF101', 'nama_mk' => 'Bengkel Pemrograman Framework', 'sks' => 3, 'semester' => 8, 'kategori' => 'Rekayasa Perangkat Lunak', 'tingkat_kesulitan' => 2],
+            ['kode_mk' => 'IF102', 'nama_mk' => 'Pemrograman Berorientasi Objek Lanjut', 'sks' => 3, 'semester' => 8, 'kategori' => 'Rekayasa Perangkat Lunak', 'tingkat_kesulitan' => 2],
+            ['kode_mk' => 'IF201', 'nama_mk' => 'Pemrograman Game', 'sks' => 3, 'semester' => 8, 'kategori' => 'Rekayasa Perangkat Lunak', 'tingkat_kesulitan' => 3],
+            ['kode_mk' => 'IF202', 'nama_mk' => 'Arsitektur Perangkat Lunak & Design Pattern', 'sks' => 3, 'semester' => 8, 'kategori' => 'Rekayasa Perangkat Lunak', 'tingkat_kesulitan' => 3],
+            ['kode_mk' => 'IF301', 'nama_mk' => 'Mobile Application Development', 'sks' => 3, 'semester' => 8, 'kategori' => 'Rekayasa Perangkat Lunak', 'tingkat_kesulitan' => 3],
+            ['kode_mk' => 'IF302', 'nama_mk' => 'Data Warehouse', 'sks' => 3, 'semester' => 8, 'kategori' => 'Kecerdasan Buatan dan Data', 'tingkat_kesulitan' => 3],
+            ['kode_mk' => 'IF303', 'nama_mk' => 'Machine Learning', 'sks' => 3, 'semester' => 8, 'kategori' => 'Kecerdasan Buatan dan Data', 'tingkat_kesulitan' => 3],
+            ['kode_mk' => 'IF401', 'nama_mk' => 'Digital Image Processing', 'sks' => 3, 'semester' => 8, 'kategori' => 'Kecerdasan Buatan dan Data', 'tingkat_kesulitan' => 4],
+            ['kode_mk' => 'IF402', 'nama_mk' => 'Pattern Recognition', 'sks' => 3, 'semester' => 8, 'kategori' => 'Kecerdasan Buatan dan Data', 'tingkat_kesulitan' => 4],
+            ['kode_mk' => 'IF403', 'nama_mk' => 'Natural Language Processing', 'sks' => 3, 'semester' => 8, 'kategori' => 'Kecerdasan Buatan dan Data', 'tingkat_kesulitan' => 4],
+            ['kode_mk' => 'IF404', 'nama_mk' => 'Keamanan Jaringan', 'sks' => 3, 'semester' => 8, 'kategori' => 'Jaringan dan Keamanan Siber', 'tingkat_kesulitan' => 2],
+            ['kode_mk' => 'IF501', 'nama_mk' => 'Digital Forensics / Network Forensics', 'sks' => 3, 'semester' => 8, 'kategori' => 'Jaringan dan Keamanan Siber', 'tingkat_kesulitan' => 5],
+            ['kode_mk' => 'IF502', 'nama_mk' => 'Cloud Computing & Virtualization', 'sks' => 3, 'semester' => 8, 'kategori' => 'Jaringan dan Keamanan Siber', 'tingkat_kesulitan' => 4],
+            ['kode_mk' => 'IF503', 'nama_mk' => 'Pemrograman IoT (Internet of Things)', 'sks' => 3, 'semester' => 8, 'kategori' => 'Jaringan dan Keamanan Siber', 'tingkat_kesulitan' => 4],
         ];
 
         foreach ($mataKuliahData as $data) {
